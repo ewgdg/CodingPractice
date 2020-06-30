b25be9d90ecb897fd928098b48e1dc2bd207c2cc
Xian Zhang <xian.z512@gmail.com>
2020-06-29 22:29:23 -0400

    migrate to junit5

---
+++ src/main/java/others/drawcardsimulation.java
@@ -0,0 +1,16 @@
+// Model 2 (M2):
+
+// P(AB) = P(both are titanic and he plays one of them) = 1/7 * 1/7 = 1/49
+// P(B) = P(at least one of card is titanic and he just happened to play the titanic) = P(he plays a random card which just happened to be titanic)  (associative law)  = ½ * 1/7 + ½ * 1/7   = 1/7
+// P(A|B) = P(AB)/P(B) = 1/7
+
+// Challenge Question 2 under M2:
+// What if player1 never play the card but you just know that one of it is titanic:
+
+// P(AB) = P(both are titanic and he plays one of them) = 1/7 * 1/7 = 1/49
+// P(B) = P(at least one of card is titanic) = 1 - 6/7*6/7 = 13/49
+// P(A|B) = P(AB)/P(B) = 1/13
+
+public class Test {
+
+}
